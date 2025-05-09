53320054a59093215f1fa3ac5f01b420a9e8222d
Valk <sebastianbelle074@protonmail.com>
2025-05-09 01:36:11 -0400

    Initial commit

---
+++ Scripts/Core/Visual Types/VisualColor.cs
@@ -0,0 +1,35 @@
+﻿using Godot;
+using GodotUtils;
+
+namespace Template;
+
+public static partial class VisualControlTypes
+{
+    private static VisualControlInfo VisualColor(VisualControlContext context)
+    {
+        Color initialColor = (Color)context.InitialValue;
+
+        GColorPickerButton colorPickerButton = new(initialColor);
+        colorPickerButton.OnColorChanged += color => context.ValueChanged(color);
+
+        return new VisualControlInfo(new ColorPickerButtonControl(colorPickerButton));
+    }
+}
+
+public class ColorPickerButtonControl(GColorPickerButton colorPickerButton) : IVisualControl
+{
+    public void SetValue(object value)
+    {
+        if (value is Color color)
+        {
+            colorPickerButton.Control.Color = color;
+        }
+    }
+
+    public Control Control => colorPickerButton.Control;
+
+    public void SetEditable(bool editable)
+    {
+        colorPickerButton.Control.Disabled = !editable;
+    }
+}
---
+++ Scripts/Core/Visual Types/VisualEnum.cs
@@ -0,0 +1,32 @@
+﻿using Godot;
+using GodotUtils;
+using System;
+
+namespace Template;
+
+public static partial class VisualControlTypes
+{
+    private static VisualControlInfo VisualEnum(Type type, VisualControlContext context)
+    {
+        GOptionButtonEnum optionButton = new(type);
+        optionButton.Select(context.InitialValue);
+        optionButton.OnItemSelected += item => context.ValueChanged(item);
+
+        return new VisualControlInfo(new OptionButtonEnumControl(optionButton));
+    }
+}
+
+public class OptionButtonEnumControl(GOptionButtonEnum optionButton) : IVisualControl
+{
+    public void SetValue(object value)
+    {
+        optionButton.Select(value);
+    }
+
+    public Control Control => optionButton.Control;
+
+    public void SetEditable(bool editable)
+    {
+        optionButton.Control.Disabled = !editable;
+    }
+}
